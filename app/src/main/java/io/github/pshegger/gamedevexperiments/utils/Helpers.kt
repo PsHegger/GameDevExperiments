0b8b5d114950236233ff444ad1ca5ea234b5a62b
gergelyhegedus <pshegger@gmail.com>
2018-05-07 10:59:34 +0200

    small code improvement in neighbors calculation

--- app/src/main/java/io/github/pshegger/gamedevexperiments/utils/Helpers.kt
+++ app/src/main/java/io/github/pshegger/gamedevexperiments/utils/Helpers.kt
@@ -3,15 +3,13 @@ package io.github.pshegger.gamedevexperiments.utils
 /**
  * @author pshegger@gmail.com
  */
-fun List<Edge>.neighbors(v: Vector): List<Vector> = this.map {
-    if (it.start == v) {
-        it.end
-    } else if (it.end == v) {
-        it.start
-    } else {
-        null
+fun List<Edge>.neighbors(v: Vector): List<Vector> = this.mapNotNull {
+    when (v) {
+        it.start -> it.end
+        it.end -> it.start
+        else -> null
     }
-}.filterNotNull()
+}
 
 fun <T> List<T>.random(): T = this[Math.floor(Math.random() * this.size).toInt()]
 operator fun <T> List<T>.times(o: List<T>) = this.flatMap { a -> o.map { b -> kotlin.collections.listOf(a, b) } }
